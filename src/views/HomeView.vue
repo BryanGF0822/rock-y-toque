784e1956eb7e6a60fed923718541a0584c0d1d1f
Bryan Guapacha <bryanicesi0822@gmail.com>
2022-09-18 16:23:25 -0500

    El sitio esta limpio.
    Ya el sitio esta listo para implemetar nuevas pantallas y hacer la pagina principal.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hola soy Home</h1>
+  <div>
+    
+  </div>
 </template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+</style>
